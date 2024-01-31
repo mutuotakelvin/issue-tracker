74155f4ab4dbb6ba8121f23ef990a6ef9ce20774
kelvin Mutuota <kelvinmutuota749@gmail.com>
2024-01-31 10:12:13 +0300

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,12 +5,13 @@ import SimpleMDE from "react-simplemde-editor";
 import {useForm, Controller, set} from 'react-hook-form'
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/ValidationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { useRouter } from 'next/navigation';
 
 const IssueForm =  z.infer<typeof createIssueSchema>
 
@@ -26,6 +27,8 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   })
   const [error, setError] = useState('')
+  const [isSubmitting, setIsSubmiting] = useState(false)
+
   return (
     <div className=''>
       { 
@@ -37,9 +40,11 @@ const NewIssuePage = () => {
       <form className='space-y-3'
           onSubmit={handleSubmit(async(data) => {
             try {
-              await axios.post('api/issues',data)
+              setIsSubmiting(true)
+              await axios.post('/api/issues',data)
               router.push('/issues')
             } catch (error) {
+              setIsSubmiting(false)
               setError('Unexpetected error occured')
             }
           })}>
@@ -55,7 +60,7 @@ const NewIssuePage = () => {
             )}
           />
           <ErrorMessage> {errors.description?.message}</ErrorMessage>
-          <Button>Submit New Issue</Button>
+          <Button disabled={isSubmitting}>Submit New Issue { isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   )
